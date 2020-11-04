fea78f3ccccb58af8f7cc87a0c30874bbf07f6b0
MUsurf <muengrsurf@missouri.edu>
2020-11-04 17:59:04 -0600

    updated testRC.py from testing

--- testRC.py
+++ testRC.py
@@ -1,6 +1,7 @@
 import pyglet
 from pyglet.gl import *
 from pymavlink import mavutil
+import time
 
 joysticks = pyglet.input.get_joysticks()
 assert joysticks, 'No joystick device is connected'
@@ -8,6 +9,8 @@ joystick = joysticks[0]
 joystick.open()
 master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 master.wait_heartbeat()
+
+
 
 window = pyglet.window.Window()
 main_batch = pyglet.graphics.Batch()
@@ -20,25 +23,29 @@ master.mav.command_long_send(
     1, 0, 0, 0, 0, 0, 0)
 
 def set_rc_channel_pwm(channel_id, pwm=1500):
-    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values = [65535 for _ in range(8)]
     rc_channel_values[channel_id - 1] = pwm
     master.mav.rc_channels_override_send(
         master.target_system,
         master.target_component,
         *rc_channel_values)
+   
+
 
 @window.event
 def on_draw():
     #running a bunch of different values, if all motors running at different speeds, it works!
+
     print(joystick.y)
-    set_rc_channel_pwm(1, 1200)
-    set_rc_channel_pwm(2, 1300)
-    set_rc_channel_pwm(3, 1400)
+    set_rc_channel_pwm(1, 1500)
+    set_rc_channel_pwm(2, 1500)
+    set_rc_channel_pwm(3, 1500)
     set_rc_channel_pwm(4, 1500)
     set_rc_channel_pwm(5, 1600)
-    set_rc_channel_pwm(6, 1700)
-    set_rc_channel_pwm(7, 1800)
-    set_rc_channel_pwm(8, 1900)
+    set_rc_channel_pwm(6, 1500)
+    set_rc_channel_pwm(7, 1500)
+    set_rc_channel_pwm(8, 1500)
+
 
     
 
