ab6c019f88c24420965e58a2b52112350b664122
Masonrf <masonrfleck@gmail.com>
2021-03-25 17:42:37 -0500

    ADC Read speedups
    Went from about 747kS/s to 860~875kS/s with some code optimizations and reading off an oscope

--- hydrophones-mcu/hydrophones-mcu.ino
+++ hydrophones-mcu/hydrophones-mcu.ino
@@ -2,12 +2,11 @@
  * Known working settings:
  * Board:      Teensy 4.1
  * MCU Freq:   720MHz
- * Optimizer:  Fastest
- * 
+ * Optimizer:  Fastest, Faster
+ * Result: 860~875kS/s
  * 
  * This doesnt result in exactly 1MS/s, but it should still be plenty for the 25kHz-40kHz
- * range of signals we are looking for anyways. This should be about 747kS/s
- * If you really wanted to get as close to 1MS/s
+ * range of signals we are looking for anyways. If you really wanted to get as close to 1MS/s
  * As you could, you should probably drop the SPI library altogether and program every piece.
  * Also overclocking a bit helps, however much higher than 720MHz you will probably want a heatsink.
  */
@@ -16,21 +15,19 @@
 #include <SPI.h>
 
 #define CS_PIN 10
-#define MAX_BUFF_SIZE 8192
+#define MAX_BUFF_SIZE 4096
 
-// ADC Timings in ns for 1Msps
-#define T_CYCLE  1000
-#define T_ACQ    290 // T_QUIET and T_EN included in here @DVIO = 3.3V
-#define T_QUIET  (10)
-#define T_EN     10
-// Scope tested for the 30ns offset
-#define T_CNV    (710 + 30)
+// Scope tested for this value. This is probably only valid for the settings used in the comments. 
+// If you start getting weird spikes in the data, increase this time until it goes away
+#define WAIT_TIME 560
 
+int16_t result = 0;
 int16_t buff[MAX_BUFF_SIZE] = {0};
 
-void printBuff();
-void fillBuff();
-int16_t readHydrophone();
+SPISettings hydrophoneA(60000000, MSBFIRST, SPI_MODE1);  // 60MHz, MSB first, Read data on falling edge of SCLK. SPI mode may be wrong. try 0 if not 1
+
+FASTRUN void printBuff();
+FASTRUN inline void fillBuff() __attribute__((always_inline));
 
 void setup() {
     Serial.begin(9600);
@@ -41,8 +38,7 @@ void setup() {
 
     delay(200); // Probably should recalibrate here
 }
-
-void loop() {
+FASTRUN void loop() {
     fillBuff();
 
     printBuff();
@@ -50,35 +46,24 @@ void loop() {
     delay(1000);  
 }
 
-// ADC Outputs a 16 bit binary twos complement.
-int16_t readHydrophone() {
-    static int16_t result = 0;
-    
-    SPI.beginTransaction(SPISettings(60000000, MSBFIRST, SPI_MODE1));  // 60MHz, MSB first, Read data on falling edge of SCLK. SPI mode may be wrong. try 0 if not 1
-    //delayNanoseconds(T_QUIET);
-    digitalWriteFast(CS_PIN, HIGH); // Initiate conversion start
-    delayNanoseconds(T_CNV);
-    
-    digitalWriteFast(CS_PIN, LOW); // Enable the ADC output
-    //delayNanoseconds(T_EN);
-    
-    result = SPI.transfer16(0x00); // Just send a zero since ADC doesn't actually read data from MCU
-    SPI.endTransaction();
-    
-    //delayNanoseconds(T_QUIET);
-  
-    return result;
-}
 
-
-void fillBuff() { // Should probably not use the global var here but oh well
+void fillBuff() {
+    noInterrupts();
     for(uint16_t i = 0; i < MAX_BUFF_SIZE; i++) {
-        buff[i] = readHydrophone();
+        digitalWriteFast(CS_PIN, HIGH); // Initiate conversion start
+        SPI.beginTransaction(hydrophoneA);
+        delayNanoseconds(WAIT_TIME);
+        
+        digitalWriteFast(CS_PIN, LOW); // Enable the ADC output
+        
+        buff[i] = SPI.transfer16(0x00); // Just send a zero since ADC doesn't actually read data from MCU;
+        SPI.endTransaction();
     }
+    interrupts();
 }
 
 // Use serial monitor to view the waveform
-void printBuff() { // Should probably not use the global var here but oh well
+void printBuff() {
     for(uint16_t i = 0; i < MAX_BUFF_SIZE; i++) {
         Serial.println(buff[i]);
     }
